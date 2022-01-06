37bf6f10778bf08fd33bda070a8971c07cc0d1a1
tamaak <jlackore@gmail.com>
2022-01-06 12:22:13 -0600

    Adding more named functions to improve readability

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -17,42 +17,43 @@ const SULFURAS = 'Sulfuras, Hand of Ragnaros';
 const BACKSTAGE_PASSES = 'Backstage passes to a TAFKAL80ETC concert';
 
 function update_quality() {
-  function update_normal_resources(item) {
-    item.sell_in = item.sell_in - 1;
-    switch (item.name) {
-      case BACKSTAGE_PASSES: {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1
-          if (item.quality < 50 && item.sell_in < 5) {
-            item.quality = item.quality + 1
-          }
-          if (item.quality < 50 && item.sell_in < 10) {
-            item.quality = item.quality + 1
-          }
-        }
-        if (item.sell_in < 0) {
-          item.quality = 0;
-        }
+  function updateBackstagePasses(item) {
+    if (item.quality < 50) {
+      item.quality = item.quality + 1
+      if (item.quality < 50 && item.sell_in < 5) {
+        item.quality = item.quality + 1
       }
-        break;
-      case AGED_BRIE: {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1
-          if (item.quality < 50 && item.sell_in < 0) {
-            item.quality = item.quality + 1;
-          }
-        }
-      }
-        break;
-      default: {
-        if (item.quality > 0) {
-          item.quality = item.quality - 1
-          if (item.quality > 0 && item.sell_in < 0) {
-            item.quality = item.quality - 1
-          }
-        }
+      if (item.quality < 50 && item.sell_in < 10) {
+        item.quality = item.quality + 1
       }
     }
+    if (item.sell_in < 0) {
+      item.quality = 0;
+    }
+  }
+
+  function updateAgedBrie(item) {
+    if (item.quality < 50) {
+      item.quality = item.quality + 1
+      if (item.quality < 50 && item.sell_in < 0) {
+        item.quality = item.quality + 1;
+      }
+    }
+  }
+
+  function updateBasicItem(item) {
+    if (item.quality > 0) {
+      item.quality = item.quality - 1
+      if (item.quality > 0 && item.sell_in < 0) {
+        item.quality = item.quality - 1
+      }
+    }
+  }
+
+  function updateConjuredItem(startingQuality, item) {
+    const normalAmountDegraded = startingQuality - item.quality;
+    const totalDegraded = normalAmountDegraded > 0 ? item.quality - normalAmountDegraded : item.quality;
+    item.quality = totalDegraded >= 0 ? totalDegraded : 0
   }
 
   items.forEach((item) => {
@@ -60,11 +61,16 @@ function update_quality() {
       return;
     }
     const startingQuality = item.quality;
-    update_normal_resources(item);
+    item.sell_in = item.sell_in - 1;
+    switch (item.name) {
+      case BACKSTAGE_PASSES: updateBackstagePasses(item);
+      break;
+      case AGED_BRIE: updateAgedBrie(item);
+      break;
+      default: updateBasicItem(item);
+    }
     if (item instanceof ConjuredItem) {
-      const normalAmountDegraded = startingQuality - item.quality;
-      const totalDegraded = normalAmountDegraded > 0 ? item.quality - normalAmountDegraded : item.quality;
-      item.quality = totalDegraded >= 0 ? totalDegraded : 0
+      updateConjuredItem(startingQuality, item);
     }
   });
 }
